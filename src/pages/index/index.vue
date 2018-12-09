5c944764380c6e9b53ce3e479efeba653e49f7ba
wangpingping <2515627499@qq.com>
2018-12-09 20:10:46 +0800

    首页顶部搜索框布局&轮播图 字体图标整合

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,33 +1,80 @@
 <template>
-  <div>
-    首页
-
-
+  <div class="index">
+    <div class="nav">
+      <i class="iconfont icon-sousuo"></i>
+      <input type="text" placeholder="搜索">
+    </div>
+    <div class="swiper">
+      <swiper indicator-dots autoplay circular>
+          <block v-for="(item, index) in swiperList" :key="index">
+              <swiper-item>
+                  <image mode="aspectFill" :src="item.image_src"></image>
+              </swiper-item>
+          </block>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import hxios from "../../utils/index";
 export default {
   data () {
     return {
-     
+     swiperList:[]
     }
   },
 
 
 
   methods: {
-  
+    
   },
 
   created () {
-    
+    hxios.get("api/public/v1/home/swiperdata")
+    .then(res=>{
+      this.swiperList=res.data.message
+    })
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  $uRed:#ff2d4a;
+  .nav{
+    background-color:$uRed;
+    padding: 20px 16px;
+    position: fixed;
+    width: 100%;
+    left: 0;
+    top: 0;
+    box-sizing: border-box;
+    input{
+      background-color: white;
+      height: 60px;
+      text-align: center;
+      border-radius: 10px;
+    }
+    i.icon-sousuo{
+      color: #bbb;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-220%,-50%)
+
+      
+    }
+  }
+  
+  .index{
+    margin-top: 100px;
+  }
+  .swiper{
+    image{
+       width: 100%;
+    } 
+   
+  }
 
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,24 @@
-function formatNumber (n) {
-  const str = n.toString()
-  return str[1] ? str : `0${str}`
+
+let baseUrl="https://autumnfish.cn/wx/"
+let hxios={
+  get(url){
+   return new Promise((resolve,reject)=>{
+      wx.request({
+        url: baseUrl+url, //开发者服务器接口地址",
+        method: 'GET',
+        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        success: res => {
+         resolve(res)
+        },
+        // 失败的回调函数
+        fail: () => {
+          reject()
+        },
+        // 完成后的回调函数
+        complete: () => {}
+      });
+    })
+  }
 }
 
-export function formatTime (date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  const t1 = [year, month, day].map(formatNumber).join('/')
-  const t2 = [hour, minute, second].map(formatNumber).join(':')
-
-  return `${t1} ${t2}`
-}
-
-export default {
-  formatNumber,
-  formatTime
-}
+export default hxios;
