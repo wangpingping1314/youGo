9c06167d571501b1bfda0a73a94e793501ce17ff
wangpingping <2515627499@qq.com>
2018-12-09 15:34:38 +0800

    项目创建开始

---
+++ src/pages/category/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    分类
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+
+
+  methods: {
+  
+  },
+
+  created () {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    首页
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+
+
+  methods: {
+  
+  },
+
+  created () {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
